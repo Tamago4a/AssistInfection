f8314205b2d13a304d629bf9e3266a996d86cbd2
Arifloof <dustswier@gmail.com>
2020-07-30 14:36:50 -0600

    WIP

--- AssistInfection.cs
+++ AssistInfection.cs
@@ -1,51 +1,42 @@
-﻿using ServerMod2.API;
-using Smod2;
-using Smod2.API;
-using Smod2.Attributes;
-using Smod2.Config;
-using Smod2.EventHandlers;
-using Smod2.Events;
-using Smod2.EventSystem.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
 using UnityEngine;
-using UnityEngine.Networking;
 
-namespace ArithFeather.AssistInfection
-{
-	[PluginDetails(
-		author = "Arith",
-		name = "Assist Infection",
-		description = "Delays your spawn if there's a chance for SCP049 to turn you into a zombie.",
-		id = "ArithFeather.AssistInfection",
-		configPrefix = "afai",
-		version = "1.1",
-		SmodMajor = 3,
-		SmodMinor = 4,
-		SmodRevision = 0
-		)]
-	public class AssistInfection : Plugin, IEventHandlerUpdate, IEventHandlerWaitingForPlayers, IEventHandlerSetRole, IEventHandlerInfected, IEventHandlerRecallZombie
+namespace ArithFeather.AssistInfection {
+	//[PluginDetails(
+	//	author = "Arith",
+	//	name = "Assist Infection",
+	//	description = "Delays your spawn if there's a chance for SCP049 to turn you into a zombie.",
+	//	id = "ArithFeather.AssistInfection",
+	//	configPrefix = "afai",
+	//	version = "1.1",
+	//	SmodMajor = 3,
+	//	SmodMinor = 4,
+	//	SmodRevision = 0
+	//	)]
+	public class AssistInfection : Plugin<Config>
+	//IEventHandlerUpdate, IEventHandlerWaitingForPlayers, IEventHandlerSetRole, IEventHandlerInfected, IEventHandlerRecallZombie
 	{
-		[ConfigOption] private readonly bool disablePlugin = false;
-		[ConfigOption] private readonly bool displayTimer = true;
-		[ConfigOption] private readonly string timerFormat = "<size=50>{0} can be resurrected for <color=#44444>{1}</color> more seconds.</size>";
+		public override void OnEnabled() {
+			base.OnEnabled();
 
-		public override void Register() =>AddEventHandlers(this);
-		public override void OnEnable() => Info("Zombie Spawn Delay Enabled");
-		public override void OnDisable() => Info("Zombie Spawn Delay Disabled");
+			Exiled.Events.Handlers.Server.WaitingForPlayers += Server_WaitingForPlayers;
+			Timing.RunCoroutine(_Update());
+		}
+
+		public override void OnDisabled() {
+			base.OnDisabled();
+		}
 
 		private List<Infected> infectedPlayers;
 		private List<Infected> InfectedPlayers => infectedPlayers ?? (infectedPlayers = new List<Infected>(20));
 		private float infectedTimer;
 
-		private class Infected
-		{
-			public Infected(Player player, Player killer, float infectionTimer)
-			{
+		private class Infected {
+			public Infected(Player player, Player killer, float infectionTimer) {
 				Player = player;
 				Killer = killer;
 				InfectionTimer = infectionTimer;
@@ -65,169 +56,139 @@ namespace ArithFeather.AssistInfection
 
 		private bool isAPlayerDead;
 
-		public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
-		{
-			if (disablePlugin)
-			{
-				PluginManager.DisablePlugin(this);
-				return;
-			}
-
-			cachedBroadcast = GameObject.Find("Host").GetComponent<Broadcast>();
+		private void Server_WaitingForPlayers() {
 			InfectedPlayers.Clear();
 			isAPlayerDead = false;
 		}
 
-
-
+		//todo Make the update more modular.
 		/// <summary>
 		/// Ticks down the zboy timers, if they reach 0, remove them, if they reach 0 and are "active", 
 		/// meaning they were supposed to spawn already, spawn them to what they were suppoed to be.
 		/// </summary>
-		public void OnUpdate(UpdateEvent ev)
-		{
-			if (!isAPlayerDead) return;
-
-			var deltaTime = Time.deltaTime;
-			var showTime = false;
-
-			if (displayTimer)
+		private IEnumerator<float> _Update() {
+			while (Config.IsEnabled)
 			{
-				infectedTimer -= deltaTime;
-				if (infectedTimer <= 0)
-				{
-					infectedTimer = 1;
-					showTime = true;
-				}
-			}
+				yield return Timing.WaitForOneFrame;
+				if (!isAPlayerDead) continue;
 
-			for (var i = InfectedPlayers.Count - 1; i >= 0; i--)
-			{
-				try
-				{
-					var zboy = InfectedPlayers[i];
-					var player = zboy.Player;
+				var deltaTime = Time.deltaTime;
+				var showTime = false;
 
-					zboy.InfectionTimer -= deltaTime;
-
-					if (showTime && zboy.InfectionTimer >= 0)
-					{
-						PersonalBroadcast(zboy.Killer, 1, string.Format(timerFormat, zboy.Player.Name, (int)zboy.InfectionTimer));
+				if (Config.DisplayTimer) {
+					infectedTimer -= deltaTime;
+					if (infectedTimer <= 0) {
+						infectedTimer = 1;
+						showTime = true;
 					}
+				}
 
-					var go = (zboy.Killer.GetGameObject() as GameObject);
-					var hpPerc = go.GetComponent<PlayerStats>().GetHealthPercent();
-					var recallSpeed = (zboy.Killer.GetGameObject() as GameObject).GetComponent<Scp049PlayerScript>().boost_recallTime.Evaluate(hpPerc);
+				for (var i = InfectedPlayers.Count - 1; i >= 0; i--) {
+					try {
+						var zboy = InfectedPlayers[i];
+						var player = zboy.Player;
 
-					if (zboy.InfectionTimer <= -recallSpeed)
-					{
+						zboy.InfectionTimer -= deltaTime;
+
+						if (showTime && zboy.InfectionTimer >= 0) {
+							PersonalBroadcast(zboy.Killer, 1,
+								string.Format(Config.TimerFormat, zboy.Player.Nickname, (int)zboy.InfectionTimer));
+						}
+
+						var go = zboy.Killer.GameObject;
+						var hpPerc = go.GetComponent<PlayerStats>().GetHealthPercent();
+						var recallSpeed = go.GetComponent<Scp049PlayerScript>().boost_recallTime.Evaluate(hpPerc);
+
+						if (zboy.InfectionTimer <= -recallSpeed) {
+							InfectedPlayers.RemoveAt(i);
+
+							if (InfectedPlayers.Count == 0) {
+								isAPlayerDead = false;
+							}
+
+							if (zboy.ShouldHaveSpawned) {
+								zboy.Player.ChangeRole(zboy.SavedRole);
+							}
+						}
+					} catch // Catch disconnected players
+					  {
 						InfectedPlayers.RemoveAt(i);
 
-						if (InfectedPlayers.Count == 0)
-						{
+						if (InfectedPlayers.Count == 0) {
 							isAPlayerDead = false;
 						}
-
-						if (zboy.ShouldHaveSpawned)
-						{
-							zboy.Player.ChangeRole(zboy.SavedRole);
-						}
-					}
-				}
-				catch // Catch disconnected players
-				{
-					InfectedPlayers.RemoveAt(i);
-
-					if (InfectedPlayers.Count == 0)
-					{
-						isAPlayerDead = false;
 					}
 				}
 			}
-		}
 
-		/// <summary>
-		/// If the player 
-		/// </summary>
-		/// <param name="ev"></param>
-		public void OnSetRole(PlayerSetRoleEvent ev)
-		{
-			if (!isAPlayerDead || ev.Role == Role.SPECTATOR) return;
+			/// <summary>
+			/// If the player 
+			/// </summary>
+			/// <param name="ev"></param>
+			public void OnSetRole(PlayerSetRoleEvent ev) {
+				if (!isAPlayerDead || ev.Role == Role.SPECTATOR) return;
 
-			for (int i = InfectedPlayers.Count - 1; i >= 0; i--)
-			{
-				var zBoy = InfectedPlayers[i];
+				for (int i = InfectedPlayers.Count - 1; i >= 0; i--) {
+					var zBoy = InfectedPlayers[i];
 
-				if (zBoy.Player.PlayerId == ev.Player.PlayerId)
-				{
-					zBoy.ShouldHaveSpawned = true;
-					zBoy.SavedRole = ev.Role;
+					if (zBoy.Player.PlayerId == ev.Player.PlayerId) {
+						zBoy.ShouldHaveSpawned = true;
+						zBoy.SavedRole = ev.Role;
 
-					ev.Role = Role.SPECTATOR;
+						ev.Role = Role.SPECTATOR;
 
-					return;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Called when player dies to 049. Saves that player to make sure they can't spawn before the infection runs out.
-		/// </summary>
-		/// <param name="ev"></param>
-		public void OnPlayerInfected(PlayerInfectedEvent ev)
-		{
-			var scp = ev.Attacker;
-
-			if (isAPlayerDead)
-			{
-				// Try to find
-				var infectedCount = InfectedPlayers.Count;
-				for (int i = 0; i < infectedCount; i++)
-				{
-					var infectedPlayer = InfectedPlayers[i];
-					if (infectedPlayer.Killer.PlayerId == scp.PlayerId)
-					{
-						InfectedPlayers[i] = new Infected(ev.Player, scp, ev.InfectTime);
-
-						if (infectedPlayer.ShouldHaveSpawned)
-						{
-							infectedPlayer.Player.ChangeRole(infectedPlayer.SavedRole);
-						}
 						return;
 					}
 				}
 			}
 
-			InfectedPlayers.Add(new Infected(ev.Player, scp, ev.InfectTime));
-			isAPlayerDead = true;
-		}
+			/// <summary>
+			/// Called when player dies to 049. Saves that player to make sure they can't spawn before the infection runs out.
+			/// </summary>
+			/// <param name="ev"></param>
+			public void OnPlayerInfected(PlayerInfectedEvent ev) {
+				var scp = ev.Attacker;
 
-		private readonly FieldInfo cachedPlayerConnFieldInfo = typeof(SmodPlayer).GetField("conn", BindingFlags.NonPublic | BindingFlags.Instance);
+				if (isAPlayerDead) {
+					// Try to find
+					var infectedCount = InfectedPlayers.Count;
+					for (int i = 0; i < infectedCount; i++) {
+						var infectedPlayer = InfectedPlayers[i];
+						if (infectedPlayer.Killer.PlayerId == scp.PlayerId) {
+							InfectedPlayers[i] = new Infected(ev.Player, scp, ev.InfectTime);
+
+							if (infectedPlayer.ShouldHaveSpawned) {
+								infectedPlayer.Player.ChangeRole(infectedPlayer.SavedRole);
+							}
+							return;
+						}
+					}
+				}
+
+				InfectedPlayers.Add(new Infected(ev.Player, scp, ev.InfectTime));
+				isAPlayerDead = true;
+			}
+
+private readonly FieldInfo cachedPlayerConnFieldInfo = typeof(SmodPlayer).GetField("conn", BindingFlags.NonPublic | BindingFlags.Instance);
 		private Broadcast cachedBroadcast;
-		private void PersonalBroadcast(Player player, uint duration, string message)
-		{
+		private void PersonalBroadcast(Player player, uint duration, string message) {
 			var connection = cachedPlayerConnFieldInfo.GetValue(player) as NetworkConnection;
 
-			if (connection == null)
-			{
+			if (connection == null) {
 				return;
 			}
 
 			cachedBroadcast.CallTargetAddElement(connection, message, duration, false);
 		}
 
-		public void OnRecallZombie(PlayerRecallZombieEvent ev)
-		{
-			for (var i = InfectedPlayers.Count - 1; i >= 0; i--)
-			{
+		public void OnRecallZombie(PlayerRecallZombieEvent ev) {
+			for (var i = InfectedPlayers.Count - 1; i >= 0; i--) {
 				var infected = infectedPlayers[i];
 
-				if (infected.Player.PlayerId == ev.Target.PlayerId)
-				{
+				if (infected.Player.PlayerId == ev.Target.PlayerId) {
 					InfectedPlayers.RemoveAt(i);
 
-					if (InfectedPlayers.Count == 0)
-					{
+					if (InfectedPlayers.Count == 0) {
 						isAPlayerDead = false;
 					}
 
---
+++ Config.cs
@@ -0,0 +1,11 @@
+﻿using Exiled.API.Interfaces;
+
+namespace ArithFeather.AssistInfection {
+	public class Config : IConfig{
+		public bool IsEnabled { get; set; } = true;
+		public bool DisplayTimer { get; set; } = true;
+
+		public string TimerFormat { get; set; } =
+			"<size=50>{0} can be resurrected for <color=#44444>{1}</color> more seconds.</size>";
+	}
+}
